ffef2a1514f969198dec53e2cd6f3bc47ed26c35
NightDarkness <nightdarkness62@outlook.com>
2022-12-10 01:57:21 -0600

    beta 1.3

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,49 +5,90 @@ function sleep(ms) {
 function reload_content(delay) {
     setInterval(async function () {
 
-        let response = await fetch("account.json", { mode: 'no-cors' });
-        let accdata = await response.json();
+        const url_values = window.location.search;
+        const url_params = new URLSearchParams(url_values);
 
-        let user = accdata["user"],
-            key = accdata["key"],
-            url = "http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=" + user + "&api_key=" + key + "&limit=1&format=json",
+        let user = url_params.get("user"),
+            url = "https://api.listenbrainz.org/1/user/" + user + "/playing-now",
             data,
             song = {
+                platform: "assets/img/logo.png",
+                id: "",
+                status: "undefined",
                 artist_name: "NO SONG - " + user,
                 track_name: "MusTrack",
-                img_link: "assets/img/logo.png"
+                img_link: "assets/img/logo.png",
+                logo:"assets/img/platform.png"
             };
 
         response = await fetch(url);
 
-        if (response.status === 200) {
+        if(response.status === 200) {
 
             data = await response.json();
+            //console.log(data);
 
-            console.log(data);
+            if(data["payload"]["listens"].length < 1) {
 
-            song["artist_name"] = data["recenttracks"]["track"][0]["artist"]["#text"];
-            song["track_name"] = data["recenttracks"]["track"][0]["name"];
-            song["img_link"] = data["recenttracks"]["track"][0]["image"][3]["#text"];
-
-            if (song["img_link"] === "https://lastfm.freetls.fastly.net//i//u//300x300//2a96cbd8b46e442fc41c2b86b821562f.png" || song["img_link"] === "https://lastfm.freetls.fastly.net/i/u/300x300/2a96cbd8b46e442fc41c2b86b821562f.png" || song["img_link"] === "") {
-
-                response = await fetch("http://ws.audioscrobbler.com/2.0/?method=user.getinfo&user=" + user + "&api_key=" + key + "&format=json");
+                response = await fetch("https://api.listenbrainz.org/1/user/" + user + "/listens?count=1");
                 data = await response.json();
-                song["img_link"] = data["user"]["image"][3]["#text"];
+                song["status"] = "last song";   
+
+            } else {
+                song["status"] = "playing now";
             }
 
-            if (song["track_name"].length > 28) {
+            song["platform"] = data["payload"]["listens"][0]["track_metadata"]["additional_info"]["music_service_name"];
+            song["artist_name"] = data["payload"]["listens"][0]["track_metadata"]["artist_name"];
+            song["track_name"] = data["payload"]["listens"][0]["track_metadata"]["track_name"];
+            song["id"] = data["payload"]["listens"][0]["track_metadata"]["additional_info"]["origin_url"];
+
+            switch(song["platform"]) {
+
+                case "YouTube Music":
+                        
+                    song["id"] = song["id"].substr(0,45);
+                    song["id"] = song["id"].substr(34,45);
+                    song["img_link"] = "https://img.youtube.com/vi/" + song["id"] + "/mqdefault.jpg";
+                    song["platform"] = "assets/img/ytm.png";
+                    break;
+
+                case 'YouTube':
+                        
+                    song["id"] = song["id"].substr(17,100);
+                    song["img_link"] = "https://img.youtube.com/vi/" + song["id"] + "/mqdefault.jpg";
+                    song["platform"] = "assets/img/yt.png";
+                    break;
+
+                case "Spotify":
+
+                    response = await fetch("https://open.spotify.com/oembed?url=" + song["id"]);
+                    data = await response.json();
+
+                    song["img_link"] = data["thumbnail_url"];
+                    song["platform"] = "assets/img/spotify.png";
+                    break;
+
+                case "SoundCloud":
+                    
+                    song["img_link"] = "assets/img/sndc.png";
+                    song["platform"] = "assets/img/sndc.png";
+                    break;
+
+            }
+
+            if(song["track_name"].length > 28) {
                 song["track_name"] = song["track_name"] + " " + song["track_name"];
             }
-            if (song["artist_name"].length > 28) {
+            if(song["artist_name"].length > 28) {
                 song["artist_name"] = song["artist_name"] + " " + song["artist_name"];
             }
 
         } else {
-            if (user != "") {
+            
+            if(user != null) {
                 song = {
-                    artist_name: "NO SONG - " + user,
+                    artist_name: "NO SONG - INVALID USER",
                     track_name: "MusTrack",
                     img_link: "assets/img/logo.png"
                 };
@@ -60,71 +101,44 @@ function reload_content(delay) {
             }
         }
 
-        if (
-            song["artist_name"] !=
-            document.getElementsByTagName("div")[9].textContent &&
-            song["artist_name"] != null
-        ) {
-            document
-                .getElementsByTagName("div")[3]
-                .setAttribute("class", "info animation-in");
-            document
-                .getElementsByTagName("div")[2]
-                .setAttribute("class", "frame animation-in");
+        if(song["artist_name"] != document.getElementsByTagName("span")[1].innerText && song["artist_name"] != null) {
+
+            document.getElementsByClassName("info")[0].classList.replace("animation-out", "animation-in");
+            await sleep(20);
+            document.getElementsByClassName("frame")[0].classList.replace("animation-out", "animation-in");
 
             await sleep(1000);
 
-            document
-                .getElementsByTagName("img")[0]
-                .setAttribute("src", song["img_link"]);
-            document
-                .getElementsByTagName("img")[1]
-                .setAttribute("src", song["img_link"]);
+            document.getElementsByTagName("img")[0].setAttribute("src", song["img_link"]);
+            document.getElementsByTagName("img")[1].setAttribute("src", song["img_link"]);
 
             await sleep(250);
 
-            document.getElementsByTagName("div")[7].textContent = song["track_name"];
-            document.getElementsByTagName("div")[9].textContent = song["artist_name"];
+            document.getElementsByTagName("div")[6].innerText = song["status"];
+            document.getElementsByTagName("img")[2].setAttribute("src", song["platform"]);
+            document.getElementsByTagName("span")[0].innerText = song["track_name"];
+            document.getElementsByTagName("span")[1].innerText = song["artist_name"];
 
-            if (song["track_name"].length > 28) {
-                document
-                    .getElementsByTagName("div")[7]
-                    .setAttribute("class", "text-animation");
+            if(song["track_name"].length > 28) {
+                document.getElementsByTagName("span")[0].setAttribute("class", "text animate");
+                document.getElementsByTagName("span")[0].innerText = song["track_name"] + song["track_name"] + song["track_name"];
             } else {
-                document.getElementsByTagName("div")[7].textContent = song["track_name"];
-                document.getElementsByTagName("div")[7].setAttribute("class", "");
+                document.getElementsByTagName("span")[0].setAttribute("class", "text");
             }
 
-            if (song["artist_name"].length > 28) {
-                document
-                    .getElementsByTagName("div")[9]
-                    .setAttribute("class", "text-animation");
+            if(song["artist_name"].length > 28) {
+                document.getElementsByTagName("span")[1].setAttribute("class", "text2 animate");
+                document.getElementsByTagName("span")[1].innerText = song["artist_name"] + song["artist_name"] + song["artist_name"];
             } else {
-                document.getElementsByTagName("div")[9].setAttribute("class", "");
+                document.getElementsByTagName("span")[1].setAttribute("class", "text2");
             }
 
             await sleep(200);
 
-            document
-                .getElementsByTagName("div")[2]
-                .setAttribute("class", "frame animation-out");
-            document
-                .getElementsByTagName("div")[3]
-                .setAttribute("class", "info animation-out");
+            document.getElementsByClassName("frame")[0].classList.replace("animation-in", "animation-out");
+            await sleep(20);
+            document.getElementsByClassName("info")[0].classList.replace("animation-in", "animation-out");
         }
-    }, delay);
-}
-
-function Image(delay) {
-    setInterval(async function () {
-
-        let key = "b4e00a95f01f3d08b3b05fd96a0c5708",
-            response = await fetch("http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=" + user + "&api_key=" + key + "&limit=1&format=json");
-
-        let info = await response.json();
-        console.log(info['recenttracks']['track'][0]['album']['#text']);
-        console.log(info)
-
 
     }, delay);
 }
